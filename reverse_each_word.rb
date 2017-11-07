7f1e81e1cba818c169cea32cc8ee20464df75db9
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-07 17:49:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(sentence)
   words = sentence.split(' ')
   reversed_words = []
 
-  words.each do |word|
-    reversed_words << word.reverse
+  reversed_words = words.collect do |word|
+    word.reverse
   end
 
   reversed_words.join(" ")
