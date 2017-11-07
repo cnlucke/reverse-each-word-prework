c00661877e84eb6f75533301a478278baabb8e96
Cristy Lucke <cristy.lucke@gmail.com>
2017-11-07 17:50:27 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  words = sentence.split(' ')
+  reversed_words = []
+
+  reversed_words = words.collect do |word|
+    word.reverse
+  end
+
+  reversed_words.join(" ")
+end
